d6747cc396c917039eddb9e9e2a336c2de56a749
Rayna Ratcliff <raynaratcliff@hotmail.com>
2021-08-09 17:23:50 -0400

    Prompts for password

--- script.js
+++ script.js
@@ -1,14 +1,53 @@
 // Special characters 
-characters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+var characters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 // Numeric characters
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // Alphabetical characters
-alphLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-alphaUpper =["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+//Set the requirments for input to generate the password
+function getPasswordOptions(userNumCharacters) {
+  if (isNaN(userNumCharacters)) {
+    alert("Please enter a valid number.");
+    return false;
+  } else if (parseInt(userNumCharacters) < 8) {
+    alert("Password length must be at least 8 characters.");
+    return false;
+  } else if (parseInt(userNumCharacters) >= 128) {
+    alert("Password must be less than 129 characters.");
+    return false;
+  }
+  return true;
+}
+
+//Get random characters from each chosen character type
+function getRandomElementFromArray(collection) {
+  return collection[Math.floor(Math.random() * collection.length)];
+}
+
+//Function to prompt user for password options
+function generatePassword() {
+  var userNumCharacters = prompt(
+    "How many characters do you want your password to contain?"
+  );
+  var passwordValid = getPasswordOptions(userNumCharacters);
+  if (passwordValid) {
+    var hascharacters = confirm(
+      "Click OK to confirm special characters."
+    );
+    var hasnumbers = confirm("Click OK to confirm adding numeric characters.");
+    var hasalphaLower = confirm(
+      "Click OK to confirm adding lowercase characters."
+    );
+    var hasalphaUpper = confirm(
+      "Click OK to confirm including uppercase characters."
+    );
+  }
+
 
 // Write password to the #password input
 function writePassword() {
@@ -16,8 +55,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
