fd8f64ae6b6f32bc5283fbd8ac8d0b2db50dd473
Rayna Ratcliff <raynaratcliff@hotmail.com>
2021-08-09 16:45:54 -0400

    Variables

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Special characters 
+characters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+// Numeric characters
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Alphabetical characters
+alphLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+alphaUpper =["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
