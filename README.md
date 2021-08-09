eca2d9126cc30909fd58b84ef8717fb38fff808f
Rayna Ratcliff <raynaratcliff@hotmail.com>
2021-08-09 17:50:54 -0400

    Password Function

--- README.md
+++ README.md
@@ -20,14 +20,24 @@ SO THAT I can create a strong password that provides greater security
 GIVEN I need a new, secure password
 WHEN I click the button to generate a password
 THEN I am presented with a series of prompts for password criteria
+
+Password validation to confirm, type of password, length of password upper case, lower case and special characters.
+
 WHEN prompted for password criteria
 THEN I select which criteria to include in the password
+
+Enter number from 8 to 128 for password length, select okay or cancel for upper, lower and special characters. 
+
 WHEN prompted for the length of the password
 THEN I choose a length of at least 8 characters and no more than 128 characters
+
 WHEN asked for character types to include in the password
 THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+
 WHEN I answer each prompt
 THEN my input should be validated and at least one character type should be selected
+Select will let each password type generate based on how the prompts are answered. 
+
 WHEN all prompts are answered
 THEN a password is generated that matches the selected criteria
 WHEN the password is generated
--- script.js
+++ script.js
@@ -10,14 +10,14 @@ var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M
 var generateBtn = document.querySelector("#generate");
 
 //Set the requirments for input to generate the password
-function getPasswordOptions(userNumCharacters) {
-  if (isNaN(userNumCharacters)) {
+function getPasswordOptions(userCharacters) {
+  if (isNaN(userCharacters)) {
     alert("Please enter a valid number.");
     return false;
-  } else if (parseInt(userNumCharacters) < 8) {
+  } else if (parseInt(userCharacters) < 8) {
     alert("Password length must be at least 8 characters.");
     return false;
-  } else if (parseInt(userNumCharacters) >= 128) {
+  } else if (parseInt(userCharacters) >= 128) {
     alert("Password must be less than 129 characters.");
     return false;
   }
@@ -25,16 +25,16 @@ function getPasswordOptions(userNumCharacters) {
 }
 
 //Get random characters from each chosen character type
-function getRandomElementFromArray(collection) {
-  return collection[Math.floor(Math.random() * collection.length)];
+function getRandomElementFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
 }
 
 //Function to prompt user for password options
 function generatePassword() {
-  var userNumCharacters = prompt(
+  var userCharacters = prompt(
     "How many characters do you want your password to contain?"
   );
-  var passwordValid = getPasswordOptions(userNumCharacters);
+  var passwordValid = getPasswordOptions(userCharacters);
   if (passwordValid) {
     var hascharacters = confirm(
       "Click OK to confirm special characters."
@@ -48,7 +48,65 @@ function generatePassword() {
     );
   }
 
+  //Conditional statement to check if user does not include any types of characters. Password generator ends if all four variables evaluate to false
+  if (
+    [hascharacters, hasnumbers, hasalphaUpper, hasalphaLower].includes (
+      true
+    )
+  )
+    //Array to store types of characters to include in password
+  var selectedCharacters = [];
 
+  //Array to contain one of each type of chosen character to ensure each will be used
+  var autogenCharacters = [];
+
+  //Conditional statements that add array of each type of character into array of possible characters based on user input and pushes new random character to guaranteedCharacters.
+  if (hascharacters) {
+    selectedCharacters = selectedCharacters.concat(characters);
+    autogenCharacters.push(
+      characters[Math.floor(Math.random() * characters.length)]
+    );
+  }
+  if (hasnumbers) {
+    selectedCharacters = selectedCharacters.concat(numbers);
+    autogenCharacters.push(
+      numbers[Math.floor(Math.random() * numbers.length)]
+    );
+  }
+  if (hasalphaLower) {
+    selectedCharacters = selectedCharacters.concat(alphaLower);
+    autogenCharacters.push(
+      alphaLower[
+        Math.floor(Math.random() * alphaLower.length)
+      ]
+    );
+  }
+  if (hasalphaUpper) {
+    selectedCharacters = selectedCharacters.concat(alphaUpper);
+    autogenCharacters.push(
+      alphaUpper[
+        Math.floor(Math.random() * alphaUpper.length)
+      ]
+    );
+  }
+
+  //For loop to iterate over the password length select from the random array 
+  var randomCharacters = [];
+  for (var i = 0; i < userCharacters; i++) {
+    var index = Math.floor(Math.random() * selectedCharacters.length);
+    randomCharacters.push(selectedCharacters[index]);
+  }
+  var replacedPosition = {};
+  //While loop to ensure an index position that has already been replaced with a guaranteed character 
+  while (autogenCharacters.length > 0) {
+    var replaceCharacters = Math.floor(Math.random() * randomCharacters.length);
+    if (!replacedPosition[replaceCharacters]) {
+      randomCharacters[replaceCharacters] = autogenCharacters.pop();
+      replacedPosition[replaceCharacters] = true;
+    }
+  }
+  return randomCharacters.join("");
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
